829c82d21d9c98103fcb28c918975a29a278d340
hassanrazak <76017055+hassanrazak@users.noreply.github.com>
2025-08-13 21:23:29 -0400

    build done

--- src/components/CountriesGrid.tsx
+++ src/components/CountriesGrid.tsx
@@ -6,12 +6,13 @@ const CountriesGrid = () => {
   const {filteredCountries} = useFilter();
 
   if (filteredCountries.length === 0) return <p>No Data available</p>;
+  console.log(filteredCountries)
 
   return (
     <Grid container maxWidth="xl"  spacing={{ xs: 2, sm: 3, md: 3 }}  mt={4} alignItems='stretch'>
       
       {filteredCountries.map((country) => (
-        <Grid item xs={12} sm={6} md={3} key={`${country.alpha3Code}`} sx={{display:'flex', justifyContent: "center",}}>
+        <Grid item xs={12} sm={6} md={3} key={`${country.alpha3Code}`} sx={{display:'flex', justifyContent: "center"}}>
           <CountryCard {...country} />
         </Grid>
       ))}
